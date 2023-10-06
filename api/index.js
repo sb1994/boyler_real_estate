b6e45c855e6a69912d03442ae3336a01a3826fa1
unknown <sean94@gmail.com>
2023-10-06 23:08:12 +0100

    Added inital mongo coonecction

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 import express from "express";
+import mongoose, { Mongoose } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose.connect(process.env.DB_URI).then(() => {
+  console.log("MongoDB connection successful");
+});
 const app = express();
 
 app.listen(3000, () => {
