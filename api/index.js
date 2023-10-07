1dccace64ef74039004e4ce18d12f20907f17095
unknown <sean94@gmail.com>
2023-10-07 10:41:03 +0100

    Created User model in Backend

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import mongoose, { Mongoose } from "mongoose";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 dotenv.config();
 
 mongoose.connect(process.env.DB_URI).then(() => {
   console.log("MongoDB connection successful");
 });
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
